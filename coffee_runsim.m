422fa5409373e456490b2f3fe76471df710167e0
Eric Miller <eric@legoaces.org>
2015-10-27 22:56:02 -0400

    Refactored coffee_runsim to replace coffee_paramtest

--- coffee_runsim.m
+++ coffee_runsim.m
@@ -1,27 +1,47 @@
-function [Times, Temperatures] = coffee_runsim(cream_addition_time)
+function res = coffee_runsim(cream_addition_time)
     % Define simulation parameters
     p = coffee_initial_params();
     p.cream_addition_time = cream_addition_time;
+    
+    drinkable_threshold = 320;
+
+    function [VALUE, ISTERMINAL, DIRECTION] = drinkableEvent(~, Y)
+        coffee_temp = energy_to_temperature(Y(1), p);
+        VALUE = coffee_temp - drinkable_threshold;
+        ISTERMINAL = 0;
+        DIRECTION = -1;
+    end
+    
+    % Setup events
+    options = odeset('Events', @drinkableEvent);
 
     % Calculate initial energy
     initial_coffee_heat = pi*(p.cup_d/2)^2*p.cup_h * ...
         p.coffee_dens * p.coffee_spheat * p.coffee_initial_temp;
 
     % Run simulation part 1
-    [Times1, Stocks1] = ode45(@coffee_flows, [0 p.cream_addition_time], initial_coffee_heat, [], p);
-    Temperatures1 = energy_to_temperature(Stocks1, p);
+    [Times1, Stocks1, Events] = ode45(@coffee_flows, [0 p.cream_addition_time], initial_coffee_heat, options, p);
 
+    if Events
+        res = Events(1);
+        return;
+    end
     % Model creamer addition
     energy = Stocks1(end) + p.cream_volume*p.coffee_dens*p.coffee_spheat*p.cream_temp;
     liquid_surface = pi*(p.cup_d/2)^2;
     p.cup_h = p.cup_h + p.cream_volume/liquid_surface;
+    
+    [diff, ~, ~] = drinkableEvent(Times1(end), energy);
+    if (diff < 0)
+        res = Times1(end);
+        return
+    end
 
     % Run simulation part 2
-    [Times2, Stocks2] = ode45(@coffee_flows, [Times1(end) p.simulation_length], energy, [], p);
-    Temperatures2 = energy_to_temperature(Stocks2, p);
-
-
-    % Graph results
-    Times = cat(1, Times1, Times2);
-    Temperatures = cat(1, Temperatures1, Temperatures2);
+    [~, ~, Events] = ode45(@coffee_flows, [Times1(end) p.simulation_length], energy, options, p);
+    
+    if Events
+        res = Events(1);
+        return;
+    end
 end
